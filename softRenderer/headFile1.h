3dc8a934f18f4a0e53a3b56412d2cc1f895b68e8
ChenHaiLong <antonychen818@163.com>
2018-08-19 22:36:05 +0800

    ..

--- softRenderer/headFile1.h
+++ softRenderer/headFile1.h
@@ -2,7 +2,7 @@
 #define headFile1_h
 
 
-//module1:
+//module:
 //math lib
 //1.vector
 typedef struct {
@@ -90,8 +90,139 @@ void matrix_set_lookat(matrix_t *m, const vector_t *eye, const vector_t *at, con
 void matrix_set_perspective(matrix_t *m, float fovy, float aspect, float zn, float zf);
 
 //2.16 set ortho m, left, right, bottom, top, near, far
-//2/(r-1)		0			0			0
-//
+//2/(r-l)		0			0			0
+//0				2/(t-b)		0			0
+//0				0			1/(zf-zn)	0
+//(l+r)/(l-r)	(t+b)/(t-b)	zn/(zn-zf)	1
+void matrix_set_ortho(matrix_t *m, float l, float r, float b, float t, float zn, float zf);
 
+//证明资料https://www.cnblogs.com/hisiqi/p/3155813.html
+
+//data structure
+//3. transform_t
+typedef struct {
+	matrix_t model;
+	matrix_t view;
+	matrix_t view_r;
+	matrix_t projection;
+	matrix_t vp;		//view*projection
+	matrix_t mv;		//model*view
+	matrix_t mvp;		//model*view*projection
+} transform_t;
+
+//3.1 transform update(world * view * projection)
+void transform_update(transform_t *ts);
+//3.2 transform_apply
+void transform_apply(const transform_t *ts, vector_t *y, const vector_t *x);
+//3.3 transform check cvv(v)
+int transform_check_cvv(const vector_t *v);
+//3.4 transform_homogenize(ts, y, x)
+void transform_homogenize(vector_t *y, const vector_t *x, float width, float height);
+//3.5 transform_homogenize(ts, y, x)
+void transform_homogenize(vector_t *y, const vector_t *x, float w, float width, float height);
+
+//4. geometry calculation(vertex, scanline, border, check, rect, ...)
+typedef struct {
+	float r, g, b, a;
+} color_t; 
+void color_init(color_t *c);
+void color_product(color_t *c, const color_t *a, const color_t *b);
+void color_scale(color_t *c, float k);
+void color_add(color_t *c, const color_t *a, const color_t *b);
+void color_sub(color_t *c, const color_t *a, const color_t *b);
+void color_interpolating(color_t *dest, const color_t *src1, const color_t *src2, const color_t *src3, float a, float b, float c);
+
+typedef struct {
+	char *name;
+
+	color_t ambient;
+	color_t diffuse;
+	color_t specular;
+	color_t transmittance;
+	color_t emission;
+
+	float shininess;
+	float ior;		//index of refraction折射
+	float dissolve;
+	int illum;
+
+	int pad0;
+	char *ambient_texname;
+	int ambient_tex_id;
+	char *diffuse_texname;
+	int diffuse_tex_id;
+	char *specular_texname;
+	int specular_tex_id;
+	char *specular_highlight_texname;
+	int specular_highlight_tex_id;
+	char *bump_texname;
+	int bump_tex_id;
+	char *displacement_texname;
+	int displacement_tex_id;
+	char *alpha_texname;
+	int alpha_tex_id;
+} material_t;
+#define NUM_MATERIAL 100
+extern material_t materials[NUM_MATERIAL];
+extern int material_cnt;
+
+void free_material(material_t *material);
+
+typedef struct {
+	point_t pos;
+	float constant;
+	float linear;
+	float quadratic;
+	color_t ambi;
+	color_t diff;
+	color_t spec;
+	bool shadow;
+} pointlight_t;
+#define NR_POINT_LIGHTS 100
+extern pointlight_t pointLights[NR_POINT_LIGHTS];
+extern int pointlight_cnt;
+
+typedef struct {
+	vector_t dir;
+	color_t ambi;
+	color_t diff;
+	color_t spec;
+	bool shadow;
+} dirlight_t;
+extern dirlight_t dirLight;
+
+typedef enum {
+	perspective, 
+	orthographic
+} PROJECTION;
+typedef struct {
+	//public 
+	vector_t pos;
+	vector_t front;
+	vector_t worldup;
+	matrix_t view_matrix;
+	matrix_t projection_matrix;
+	matrix_t view_matrix_r;
+	int width;
+	int height;
+	float fovy;
+	float zn;
+	float zf;
+
+	float left;
+	float right;
+	float bottom;
+	float top;
+
+	bool dirty;
+	PROJECTION projection;
+	bool main;
+
+	//private
+	float aspect;
+} camera_t;
+#define MAX_NUM_CAMERA 10
+extern camera_t cameras[MAX_NUM_CAMERA];
+extern int camera_count;
 #endif // !headFile1_h
 
